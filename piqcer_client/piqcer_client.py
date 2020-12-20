57d0a543816db59ebb14c98c90db0f80dc6954b6
jordyvanraalte <gewoonjvr@gmail.com>
2020-12-20 23:41:53 +0100

    Added all modules to client

--- piqcer_client/piqcer_client.py
+++ piqcer_client/piqcer_client.py
@@ -1,6 +1,24 @@
 from .config.config import Config
-from .resources.users import Users
+from .resources.backorders import Backorders
+from .resources.customer_fields import CustomerFields
 from .resources.customers import Customers
+from .resources.locations import Locations
+from .resources.order_fields import OrderFields
+from .resources.orders import Orders
+from .resources.picklists import Picklists
+from .resources.pricelists import PriceLists
+from .resources.product_fields import ProductFields
+from .resources.products import Products
+from .resources.purchase_orders import PurchaseOrders
+from .resources.receipts import Receipts
+from .resources.returns import Returns
+from .resources.shipping_provider import ShippingProviders
+from .resources.stock_history import StockHistory
+from .resources.suppliers import Suppliers
+from .resources.tags import Tags
+from .resources.users import Users
+from .resources.vat_group import VatGroup
+from .resources.warehouses import Warehouses
 
 
 class Client:
@@ -10,5 +28,23 @@ class Client:
         config.base_url = base_url
         config.api_key = api_key
 
-        self.users = Users()
+        self.backorders = Backorders()
+        self.customer_fields = CustomerFields()
         self.customers = Customers()
+        self.locations = Locations()
+        self.order_fields = OrderFields()
+        self.orders = Orders()
+        self.picklists = Picklists()
+        self.pricelists = PriceLists()
+        self.product_fields = ProductFields()
+        self.products = Products()
+        self.purchase_orders = PurchaseOrders()
+        self.receipts = Receipts()
+        self.returns = Returns()
+        self.shipping_provider = ShippingProviders()
+        self.stock_history = StockHistory()
+        self.suppliers = Suppliers()
+        self.tags = Tags()
+        self.users = Users()
+        self.vat_group = VatGroup()
+        self.warehouses = Warehouses()
